7981a620e39ac71deef0969355b57950e6f1bc67
SamahOud <you@example.com>
2024-07-13 11:22:10 +0300

    Login Page + Routing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,12 +13,16 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { useAuth } from '../context/Auth/AuthContext';
+import { Button, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const settings = ['Profile', 'Account', 'Dashboard', 'My Order', 'Logout'];
 
 const Navbar = () => {
-    const { username, token } = useAuth()
+    const { username, isAuthenticated } = useAuth()
     const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+    const navigate = useNavigate()
 
     const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorElUser(event.currentTarget);
@@ -27,9 +31,11 @@ const Navbar = () => {
     const handleCloseUserMenu = () => {
         setAnchorElUser(null);
     };
-    console.log("From navbar: ", { username, token});
-    
+    console.log("From navbar: ", { username, isAuthenticated });
 
+    const handleLogin = () => {
+        navigate("/login")
+    }
     return (
         <AppBar position="static">
             <Container maxWidth="xl">
@@ -54,33 +60,45 @@ const Navbar = () => {
                         </Box>
 
                         <Box sx={{ flexGrow: 0 }}>
-                            <Tooltip title="Open settings">
-                                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                                </IconButton>
-                            </Tooltip>
-                            <Menu
-                                sx={{ mt: '45px' }}
-                                id="menu-appbar"
-                                anchorEl={anchorElUser}
-                                anchorOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                keepMounted
-                                transformOrigin={{
-                                    vertical: 'top',
-                                    horizontal: 'right',
-                                }}
-                                open={Boolean(anchorElUser)}
-                                onClose={handleCloseUserMenu}
-                            >
-                                {settings.map((setting) => (
-                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                        <Typography textAlign="center">{setting}</Typography>
-                                    </MenuItem>
-                                ))}
-                            </Menu>
+                            {isAuthenticated ? <>
+                                <Tooltip title="Open settings">
+                                    <Grid container alignItems="center" justifyContent="center" gap={2}>
+                                        <Grid item>
+                                            <Typography>{username}</Typography>
+                                        </Grid>
+                                        <Grid item>
+                                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                                <Avatar alt={username || ''} src="/static/images/avatar/2.jpg" />
+                                            </IconButton>
+                                        </Grid>
+                                    </Grid>
+                                </Tooltip>
+
+                                <Menu
+                                    sx={{ mt: '45px' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    {settings.map((setting) => (
+                                        <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                            <Typography textAlign="center">{setting}</Typography>
+                                        </MenuItem>
+                                    ))}
+                                </Menu>
+                            </> : (
+                                <Button variant='contained' color='success' onClick={handleLogin}>Login</Button>
+                            )}
                         </Box>
                     </Box>
                 </Toolbar>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,10 +4,8 @@ import { BASE_URI } from "../constants/baseUrl"
 import { useAuth } from "../context/Auth/AuthContext"
 import { useNavigate } from "react-router-dom"
 
-const RegisterPage = () => {
+const LoginPage = () => {
     const [error, setError] = useState("")
-    const firstNameRef = useRef<HTMLInputElement>(null)
-    const lastNameRef = useRef<HTMLInputElement>(null)
     const emailRef = useRef<HTMLInputElement>(null)
     const passwordRef = useRef<HTMLInputElement>(null)
 
@@ -16,23 +14,22 @@ const RegisterPage = () => {
     const { login } = useAuth()
 
     const onSubmit = async () => {
-        const firstName = firstNameRef.current?.value
-        const lastName = lastNameRef.current?.value
         const email = emailRef.current?.value
         const password = passwordRef.current?.value
 
-        if (!firstName || !lastName || !email || !password) {
-            setError("All fields are required!")
+        // Validate the form data
+        if (!email || !password) {
+            setError("Check submitted data.")
             return
         }
         
         // Make the call to API to create the user  
-        const response = await fetch(`${BASE_URI}/user/register`, {
+        const response = await fetch(`${BASE_URI}/user/login`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({ firstName, lastName, email, password })
+            body: JSON.stringify({ email, password })
         })
         if (!response.ok) {
             setError("Unable to register user, please try different credintials!")
@@ -50,19 +47,17 @@ const RegisterPage = () => {
     return (
         <Container>
             <Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", mt: 4 }}>
-                <Typography variant="h5">Register New Acount</Typography>
+                <Typography variant="h5">Login to Your Acount</Typography>
 
                 <Box
                     sx={{
                         display: "flex", flexDirection: "column",
                         gap: 2, mt: 4, border: 1, borderColor: "#f5f5f5", p: 2
                     }}>
-                    <TextField inputRef={firstNameRef} label="First Name" name="firstName" />
-                    <TextField inputRef={lastNameRef} label="Last Name" name="lastName" />
                     <TextField inputRef={emailRef} label="Email" name="email" />
                     <TextField inputRef={passwordRef} type="password" label="Password" name="password" />
 
-                    <Button onClick={onSubmit} variant="contained">Register</Button>
+                    <Button onClick={onSubmit} variant="contained">Login</Button>
                     {error && <Typography variant="body2" color="error">{error}</Typography>}
                 </Box>
             </Box>
@@ -70,4 +65,4 @@ const RegisterPage = () => {
     )
 }
 
-export default RegisterPage
+export default LoginPage
